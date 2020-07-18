38480d7fa6120007557bce5f8d76fd4a43a7bce3
Nathan Sebhastian <nathansebhastian@gmail.com>
2020-07-19 01:05:24 +0700

    format code

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import Card from '@bit/nsebhastian.design-system.card'
+import React from 'react';
+import Card from '@bit/nsebhastian.design-system.card';
 
 const App = props => {
-
-	const buttonClick = () => {
-    const onClick = props.onClick
-    if (onClick){
+  const buttonClick = () => {
+    const onClick = props.onClick;
+    if (onClick) {
       onClick();
     } else {
-      console.log('button is clicked')
+      console.log('button is clicked');
     }
-  }
-		return(
-			<div style={{padding: '50px 12px', display: 'flex'}}>
-				<Card image="https://firebasestorage.googleapis.com/v0/b/react-firebase-basic.appspot.com/o/hotel1.jpg?alt=media&token=e1ffa47a-268a-42a1-8da4-8b954b9ffaa9" title="Hotel 1" buttonTitle="Book" buttonClick ={() => buttonClick()} />
-				<Card image="https://firebasestorage.googleapis.com/v0/b/react-firebase-basic.appspot.com/o/hotel2.jpg?alt=media&token=45ec4e54-d2fe-442f-8b02-a841200c7f54" title="Hotel 2" buttonTitle="Book" buttonClick ={() => buttonClick()} />
-				<Card image="https://firebasestorage.googleapis.com/v0/b/react-firebase-basic.appspot.com/o/hotel3.jpg?alt=media&token=6d10b7d1-e2b8-4c0a-bd7b-c4212530bc43" title="Hotel 3" buttonTitle="Book" buttonClick ={() => buttonClick()} />
-			</div>
-		)
-}
+  };
+  return (
+    <div style={{padding: '50px 12px', display: 'flex'}}>
+      <Card
+        image='https://firebasestorage.googleapis.com/v0/b/react-firebase-basic.appspot.com/o/hotel1.jpg?alt=media&token=e1ffa47a-268a-42a1-8da4-8b954b9ffaa9'
+        title='Hotel 1'
+        buttonTitle='Book'
+        buttonClick={() => buttonClick()}
+      />
+      <Card
+        image='https://firebasestorage.googleapis.com/v0/b/react-firebase-basic.appspot.com/o/hotel2.jpg?alt=media&token=45ec4e54-d2fe-442f-8b02-a841200c7f54'
+        title='Hotel 2'
+        buttonTitle='Book'
+        buttonClick={() => buttonClick()}
+      />
+      <Card
+        image='https://firebasestorage.googleapis.com/v0/b/react-firebase-basic.appspot.com/o/hotel3.jpg?alt=media&token=6d10b7d1-e2b8-4c0a-bd7b-c4212530bc43'
+        title='Hotel 3'
+        buttonTitle='Book'
+        buttonClick={() => buttonClick()}
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- app3/src/App.js
+++ app3/src/App.js
@@ -1,39 +1,39 @@
 import React from 'react';
-import Navbar from '@bit/nsebhastian.design-system.navbar'
+import Navbar from '@bit/nsebhastian.design-system.navbar';
 
 const ExploreHotel = React.lazy(() => import('app1/App'));
 const BookRoom = React.lazy(() => import('app2/App'));
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { view:1 }
+    this.state = {view: 1};
     this.bookTheRoom = this.bookTheRoom.bind(this);
   }
 
-  bookTheRoom(){
-    this.setState({view: 2})
+  bookTheRoom() {
+    this.setState({view: 2});
   }
-  
-  render(){
-    const {view} = this.state
+
+  render() {
+    const {view} = this.state;
     let component = (
       <React.Suspense fallback='Loading app1'>
-        <ExploreHotel onClick={this.bookTheRoom}/>
+        <ExploreHotel onClick={this.bookTheRoom} />
       </React.Suspense>
-    )
-    if(view === 2){
+    );
+    if (view === 2) {
       component = (
         <React.Suspense fallback='Loading app2'>
           <BookRoom />
         </React.Suspense>
-      )
+      );
     }
-    return(
+    return (
       <>
         <Navbar links={['home', 'about']} />
-          {component}
+        {component}
       </>
-    )
+    );
   }
 }
